4514bc25e1f1894dd61e110989f1649d8ddd497b
valerik-buharik11 <106351993+valerik-buharik11@users.noreply.github.com>
2022-05-27 00:28:38 +0300

    Add files via upload

---
+++ diya/access/values.js
@@ -0,0 +1,24 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = true; // Права
+var isCovidCertificateEnabled = true; // COVID-сертификат
+
+// Общее
+var birthdate = "31.10.2001"; // Дата рождения
+var fullname = "Лісіцина Марія Олексіївна" // ФИО
+var name = "Марія" // Имя
+
+// Паспорт
+var passport_id = "438466456" // Номер паспорта
+
+// КПП
+var kpp_id = "6644632566" // ИНН
+
+// Права
+var rights_valid_until = "11.12.2024" // Права "Дійсні до"
+var rights_categories = "A" // Права "Категорії"
+var rights_tsc = "ТСЦ 6466" // Права "Видав
+var rights_id = "KBE968639" // Права "Серія та номер""
+
+// COVID-сертификат
+var covid_valid_until = "22.04.2023" // Сертификат "Дійсний до"
+var covid_certificate_id = "URN:UVCI:01:UA:0<br>E55669376876888<br>B9E2520C4F88930" // Номер сертификата (<br> - перенос строки)
